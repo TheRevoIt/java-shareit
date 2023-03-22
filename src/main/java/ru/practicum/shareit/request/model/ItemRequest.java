a9a07f049c64f6d5f9be23d059499798e559f738
Vladimir Veretennikov <revosierra@gmail.com>
2023-03-22 23:13:00 +0100

    feat: created model and updated schema

---
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -0,0 +1,38 @@
+package ru.practicum.shareit.request.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import ru.practicum.shareit.user.model.User;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "requests")
+@NoArgsConstructor
+public class ItemRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String description;
+    @ManyToOne
+    @JoinColumn(name = "requester_id")
+    private User requester;
+    private LocalDateTime created;
+
+    ItemRequest(String description, LocalDateTime created) {
+        this.description = description;
+        this.created = created;
+    }
+}
