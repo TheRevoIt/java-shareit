7073ca8da01525cc6b8e6916b061329c02ae1b50
Vladimir Veretennikov <revosierra@gmail.com>
2023-03-22 23:12:14 +0100

    feat: created dtos for item requests

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDtoResponse.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDtoResponse.java
@@ -3,20 +3,15 @@ package ru.practicum.shareit.request.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import ru.practicum.shareit.util.Create;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @AllArgsConstructor
-public class ItemRequestDto {
+public class ItemRequestDtoResponse {
     private long id;
     private String description;
-    @NotBlank(groups = Create.class)
-    @Size(max = 255)
-    private long requesterId;
     private LocalDateTime created;
+    private long requesterId;
 }
