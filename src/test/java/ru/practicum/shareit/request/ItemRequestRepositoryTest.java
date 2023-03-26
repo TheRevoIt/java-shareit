26ccf1c1c07a4b68c4046c472876c9dabdf24e2a
Vladimir Veretennikov <revosierra@gmail.com>
2023-03-26 23:19:47 +0200

    fix: fixed tests #5

--- src/test/java/ru/practicum/shareit/request/ItemRequestRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/request/ItemRequestRepositoryTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.practicum.shareit.item.model.Item;
